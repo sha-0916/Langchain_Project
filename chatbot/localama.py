a7641fefbfef61007896177585591a65fcb86343
shubhi09 <sharma.shubhi0995@gmail.com>
2026-09-04 13:17:15 +0200

    Adaptation of the code for EU langsmith users in 2026

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -8,4 +8,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [   
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]   
+)
+
+#streamlit framework
+st.title('Langchain Demo With Ollama API')
+input_text=st.text_input("Search the topic u want")
+
+#ollama LLm
+llm=ChatOllama(model="llama3.2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
+
 
